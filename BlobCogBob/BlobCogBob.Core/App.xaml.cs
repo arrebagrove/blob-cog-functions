cd5a1fc626285e08e55551224ba028356f52d437
Matt Soucoup <msoucoup@codemilltech.com>
2018-01-23 11:35:46 -0600

    Initial add

---
+++ BlobCogBob/BlobCogBob.Core/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace BlobCogBob.Core
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MenusList());
+        }
+    }
+}
---
+++ BlobCogBob/BlobCogBob.Core/Pages/MenusList.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace BlobCogBob.Core
+{
+    public partial class MenusList : ContentPage
+    {
+        public MenusList()
+        {
+            InitializeComponent();
+        }
+    }
+}
